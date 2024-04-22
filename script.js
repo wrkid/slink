894d46a339d0f50c5ce34aaaf31ace2f25b9b7a4
wrkid <137333242+wrkid@users.noreply.github.com>
2024-04-22 13:17:35 +0300

    v1.1

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const id = 'ru.bnpl.mobile'
 const deep1 = 'market://launch?id='+id
 const deep2 = 'hiapplink://com.huawei.appmarket?appId='+id
 window.addEventListener('click', function(e) {
-    const value = e.currentTarget.getAttribute('id');
+    const value = e.target.getAttribute('id');
+    console.log(value)
     if (value === 'btn1') {
         window.location.href = deep1;
     } else if (value === 'btn2') {
