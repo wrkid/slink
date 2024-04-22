5a1c3670b8682d52f0bb2f8e24b1c9949c4fa734
wrkid <137333242+wrkid@users.noreply.github.com>
2024-04-22 13:38:37 +0300

    ссылки

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const id = 'ru.bnpl.mobile'
-const deep1 = 'market://launch?id='+id
-const deep2 = 'hiapplink://com.huawei.appmarket?appId='+id
+const id1 = 'ru.bnpl.mobile'
+const id2 = 'C109474877' 
+const deep1 = 'market://launch?id='+id1
+const deep2 = 'hiapplink://com.huawei.appmarket?appId='+id1
+const deep3 = 'appmarket://details?id='+id1
+
+const deep5 = 'market://launch?id='+id2
+const deep6 = 'hiapplink://com.huawei.appmarket?appId='+id2
+const deep7 = 'appmarket://details?id='+id2
 window.addEventListener('click', function(e) {
     const value = e.target.getAttribute('id');
     console.log(value)
@@ -8,5 +14,13 @@ window.addEventListener('click', function(e) {
         window.location.href = deep1;
     } else if (value === 'btn2') {
         window.location.href = deep2;
+    } else if (value === 'btn3') {
+      window.location.href = deep2;
+    } else if (value === 'btn4') {
+      window.location.href = deep2;
+    } else if (value === 'btn5') {
+      window.location.href = deep2;
+    } else if (value === 'btn6') {
+      window.location.href = deep2;
     }
 })
